c70dffada36c8e90ccdfe9f59707674815394c64
rem1ni <92308422+rem1ni@users.noreply.github.com>
2022-12-09 19:55:24 +0300

    add user adding

--- backend/src/main/java/com/backend/bprobackend/controller/UserController.java
+++ backend/src/main/java/com/backend/bprobackend/controller/UserController.java
@@ -1,12 +1,21 @@
 package com.backend.bprobackend.controller;
 
+import com.backend.bprobackend.model.Route;
 import com.backend.bprobackend.model.User;
 import com.backend.bprobackend.repository.UserRepos;
 
+import com.backend.bprobackend.request.RouteChangeRequest;
+import com.backend.bprobackend.request.RouteDeleteRequest;
+import com.backend.bprobackend.request.UserChangeRequest;
+import com.backend.bprobackend.request.UserDeleteRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static jdk.internal.net.http.AuthenticationFilter.encoder;
 
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -15,7 +24,8 @@ import java.util.List;
 public class UserController {
 
     private UserRepos userRepos;
-
+    @Autowired
+    PasswordEncoder encoder;
     @Autowired
     public UserController(UserRepos userRepos) {
         this.userRepos = userRepos;
@@ -29,5 +39,27 @@ public class UserController {
         return users;
     }
 
+    @PostMapping("/edit")
+    public ResponseEntity<?> editUser(@RequestBody UserChangeRequest userChangeRequest) {
+        User user = userRepos.getById(userChangeRequest.getId());
+        if (userRepos.existsByUsername(user.getUsername()))
+        {
+            return ResponseEntity.ok("fail of century");
+        }
+        else {
+            user.setUsername(userChangeRequest.getUsername());
+            user.setPassword(encoder.encode(userChangeRequest.getPassword()));
+                    userRepos.save(user);
+                    return ResponseEntity.ok("success");
+        }
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<?> deleteUser(@RequestBody UserDeleteRequest userDeleteRequest) {
+
+        userRepos.deleteById(userDeleteRequest.getId());
+
+        return ResponseEntity.ok("success");
+    }
 }
 
---
+++ backend/src/main/java/com/backend/bprobackend/request/UserDeleteRequest.java
@@ -0,0 +1,13 @@
+package com.backend.bprobackend.request;
+
+public class UserDeleteRequest {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
