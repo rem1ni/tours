2d29b0a30b52675d7583a4dc1dcee22316659175
rem1ni <92308422+rem1ni@users.noreply.github.com>
2022-12-09 20:09:51 +0300

    fix user edit

--- backend/src/main/java/com/backend/bprobackend/controller/UserController.java
+++ backend/src/main/java/com/backend/bprobackend/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.backend.bprobackend.controller;
 
+import com.backend.bprobackend.model.EnumRole;
+import com.backend.bprobackend.model.Role;
 import com.backend.bprobackend.model.Route;
 import com.backend.bprobackend.model.User;
+import com.backend.bprobackend.repository.RoleRepos;
 import com.backend.bprobackend.repository.UserRepos;
 
 import com.backend.bprobackend.request.RouteChangeRequest;
@@ -13,16 +16,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
-
+import java.util.Set;
 
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/tour/user")
 public class UserController {
-
+   @Autowired
     private UserRepos userRepos;
+    @Autowired
+    private RoleRepos roleRepos;
+
     @Autowired
     PasswordEncoder encoder;
     @Autowired
@@ -48,6 +55,15 @@ public class UserController {
         else {
             user.setUsername(userChangeRequest.getUsername());
             user.setPassword(encoder.encode(userChangeRequest.getPassword()));
+            Set<Role> roles = new HashSet<>();
+            Role userRole = roleRepos.findByName(EnumRole.ROLE_EMPLOYEE)
+                    .orElseThrow(() -> new RuntimeException("Роль не найдена"));
+            roles.add(userRole);
+            if (userChangeRequest.isAdmin() == true) {
+                Role admin = roleRepos.getById(2);
+                roles.add(admin);
+            }
+            user.setRoles(roles);
                     userRepos.save(user);
                     return ResponseEntity.ok("success");
         }
--- backend/src/main/java/com/backend/bprobackend/request/UserChangeRequest.java
+++ backend/src/main/java/com/backend/bprobackend/request/UserChangeRequest.java
@@ -1,11 +1,23 @@
 package com.backend.bprobackend.request;
 
+import java.util.Set;
+
 public class UserChangeRequest {
 
     private Long id;
     private String username;
     private String password;
 
+    private boolean admin;
+
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
 
     public Long getId() {
         return id;
